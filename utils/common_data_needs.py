2bd10c6c941eb794ad9fba21faa6d944a8f65004
jcsyme <jsyme.bus@gmail.com>
2025-07-19 01:31:30 -0600

    added elasticity estimation

--- utils/common_data_needs.py
+++ utils/common_data_needs.py
@@ -2,19 +2,71 @@
 import os, os.path
 import pandas as pd
 import pathlib
+import sisepuede.core.attribute_table as att
 import sisepuede.core.support_classes as sc
 import sisepuede.manager.sisepuede_file_structure as sfs
 import sisepuede.manager.sisepuede_models as sm
 import sisepuede.utilities._toolbox as sf
+from numpy import arange
 from typing import *
 
 
+
+
+
+    
 
 ################################
 #    SETUP GLOBAL VARIABLES    #
 ################################
 
+def get_file_structure(
+    y0: int = 2015,
+    y1: int = 2070,
+) -> Tuple[sfs.SISEPUEDEFileStructure, att.AttributeTable]:
+    """Get the SISEPUEDE File Structure and update the attribute table
+        with new years.
+    """
+    # setup some SISEPUEDE variables and update time period
+    file_struct = sfs.SISEPUEDEFileStructure()
 
+    # get some keys
+    key_time_period = file_struct.model_attributes.dim_time_period
+    key_year = file_struct.model_attributes.field_dim_year
+
+
+    ##  BUILD THE ATTRIBUTE AND UPDATE
+
+    # setup the new attribute table
+    ra = arange(0, y1 - y0 + 1, ).astype(int)
+    attribute_time_period = att.AttributeTable(
+        pd.DataFrame(
+            {
+                key_time_period: ra,
+                key_year: y0 + ra,
+            }
+        ),
+        key_time_period,
+        
+    )
+
+    # finally, update the ModelAttributes inside the file structure
+    (
+        file_struct
+        .model_attributes
+        .update_dimensional_attribute_table(
+            attribute_time_period,
+        )
+    )
+
+    # return the tuple
+    out = (file_struct, attribute_time_period, )
+
+    return out
+
+
+
+    
 # set up some paths
 _PATH_CUR = pathlib.Path(__file__).parents[0]
 _PATH_PROJ = _PATH_CUR.parents[0]
@@ -22,11 +74,8 @@ _PATH_INPUTS = _PATH_PROJ.joinpath("input_data")
 _PATH_OUTPUTS = _PATH_PROJ.joinpath("output_data")
 _PATH_BASE_RAW_DATA = _PATH_INPUTS.joinpath("sisepuede_raw_global_inputs_uganda.csv")
 
-
-# setup some SISEPUEDE variables
-_SISEPUEDE_FILE_STRUCTURE = sfs.SISEPUEDEFileStructure()
-
 # model attributes and associated support classes
+_SISEPUEDE_FILE_STRUCTURE, _ATTRIBUTE_TABLE_TIME_PERIOD = get_file_structure()
 _SISEPUEDE_MODEL_ATTRIBUTES = _SISEPUEDE_FILE_STRUCTURE.model_attributes
 _SISEPUEDE_REGIONS = sc.Regions(_SISEPUEDE_MODEL_ATTRIBUTES, )
 _SISEPUEDE_TIME_PERIODS = sc.TimePeriods(_SISEPUEDE_MODEL_ATTRIBUTES, )
@@ -44,6 +93,8 @@ _SISEPUEDE_MODELS = sm.SISEPUEDEModels(
 
 
 
+
+
 ##########################
 #    DEFINE FUNCTIONS    #
 ##########################
@@ -53,9 +104,12 @@ class MissingValuesError(Exception):
 
     
 def _build_from_outputs(
+    years_required: tuple, 
     extension_read: str = "csv",
+    fns_exclude: Union[List[str], None] = None,
     merge_type: str = "outer",
     path_csvs: pathlib.Path = _PATH_OUTPUTS,
+    print_info: bool = False,
     stop_on_error: bool = False,
     **kwargs
 ) -> pd.DataFrame:
@@ -64,15 +118,22 @@ def _build_from_outputs(
 
     Function Arguments
     ------------------
-
+    years_required : Tuple[int, int]
+        Tuple giving years that are required to run the analysis. Will
+        confirm these years are present.
+        
     Keyword Arguments
     -----------------
     extension_read : str
         Default extension to read
+    fns_exclude : Union[List[str], None]
+        Optional list of file names to exclude
     merge_type : str
         Merge type to pass to pd.merge as 'how = merge_type'
     path_csvs : pathlib.Path
         Directory storing CSVs
+    print_info : bool
+        Print info while iterating?
     stop_on_error : bool
         Stop if there's a read error? If False, skips files that produce 
         errors.
@@ -81,21 +142,36 @@ def _build_from_outputs(
     """
     # init
     global _DF_OVERWRITE
+    global _DF_CUR
+    global _PATHS_ITER
     _DF_OVERWRITE = None
+    _DF_CUR = None
+    _PATHS_ITER = []
     
     # get raw inputs
     df_base = get_raw_ssp_inputs()
 
+    shp = None
 
     # check for available files
     df_overwrite = None
     for path in path_csvs.iterdir():
+
+        # skip?
+        if isinstance(fns_exclude, list):
+            if path.parts[-1] in fns_exclude:
+                continue
+        
         # skip non-csvs
         if path.suffix != f".{extension_read}": continue
 
         # try reading the file
         try:
-            df_cur = pd.read_csv(path, **kwargs)
+            df_cur = (
+                pd.read_csv(path, **kwargs)
+                .drop_duplicates()
+            )
+            
         except Exception as e:
             msg = f"Error reading {extension_read} file at {path}: {e}"
             if stop_on_error:
@@ -104,6 +180,8 @@ def _build_from_outputs(
             warnings.warn(msg)
             continue
 
+        shp = df_cur.shape
+        
         # if successful, update the df
         df_overwrite = (
             df_cur
@@ -115,14 +193,35 @@ def _build_from_outputs(
             )
         )
 
+        _PATHS_ITER.append(path)
+        if print_info: print(f"Shape after path {path}: {df_overwrite.shape}\n")
+
 
     ##  CHECKS AND FINAL OVERWRITE
 
-    # check for NAs
-    if df_overwrite.dropna().shape != df_overwrite.shape:
+    df_overwrite = df_overwrite.dropna()
+    
+    # check that all needed years are included
+    set_years_req = set(range(years_required[0], years_required[1] + 1))
+    proceed = set_years_req.issubset(set(df_overwrite[_SISEPUEDE_TIME_PERIODS.field_year]))
+    if not proceed:
         _DF_OVERWRITE = df_overwrite
-        raise MissingValuesError(f"NAs found in df_overwite. Check the dataframe at _DF_OVERWRITE")
+        raise MissingValuesError(f"Years missing from the dataframe. Check the dataframe at _DF_OVERWRITE")
 
+    # merge df_base to set of years available and fill
+    df_base = (
+        pd.merge(
+            df_overwrite[
+                df_overwrite[_SISEPUEDE_TIME_PERIODS.field_year]
+                .isin(set_years_req)
+            ][[_SISEPUEDE_TIME_PERIODS.field_year]],
+            df_base,
+            how = "left",
+        )
+        .interpolate()
+        .bfill()
+    )
+    
     # overwrite fields in base to prouce output
     df_out = sf.match_df_to_target_df(
         df_base,
@@ -140,12 +239,15 @@ def _build_from_outputs(
             columns = _SISEPUEDE_TIME_PERIODS.field_year,
         )
     )
+
+    if _SISEPUEDE_REGIONS.key in df_out.columns:
+        df_out.drop(columns = _SISEPUEDE_REGIONS.key, inplace = True, )
     
     
     return df_out
 
 
-    
+
 def get_raw_ssp_inputs(
 ) -> pd.DataFrame:
     """Retrieve the base, raw Uganda inputs for SISEPUEDE,
