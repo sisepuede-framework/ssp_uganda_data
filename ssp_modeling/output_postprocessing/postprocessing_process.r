31fae6e580e6b6329cd78259d64daf7c47920e5f
fabian fuentes <cffuentes@tec.mx>
2025-08-15 18:54:54 -0600

    update 250815

--- ssp_modeling/output_postprocessing/postprocessing_process.r
+++ ssp_modeling/output_postprocessing/postprocessing_process.r
@@ -10,17 +10,17 @@ library(ggplot2)
 
 rm(list=ls())
 
-#setwd('/Users/fabianfuentes/git/ssp_uganda_data/')
+setwd('/Users/fabianfuentes/git/ssp_uganda_data/')
 
 #ouputfile
-dir.output  <- "ssp_modeling/ssp_run_output/sisepuede_run_2025-08-11T10;35;37.250554/"
-output.file <-"sisepuede_run_2025-08-11T10;35;37.250554.csv"
+dir.output  <- "ssp_modeling/ssp_run_output/sisepuede_run_2025-08-15T17;53;15.651893/"
+output.file <-"sisepuede_run_2025-08-15T17;53;15.651893.csv"
 
 region <- "uganda" 
 iso_code3 <- "UGA"
 
-#source('ssp_modeling/output_postprocessing/scr/run_script_baseline_run_new.r')
+source('ssp_modeling/output_postprocessing/scr/run_script_baseline_run_new.r')
 
-source('ssp_modeling/output_postprocessing/scr/data_prep_new_mapping_uganda_raw.r')
+source('ssp_modeling/output_postprocessing/scr/data_prep_new_mapping_uganda.r')
 
 source('ssp_modeling/output_postprocessing/scr/data_prep_drivers.r')
--- ssp_modeling/output_postprocessing/scr/run_script_baseline_run_new.r
+++ ssp_modeling/output_postprocessing/scr/run_script_baseline_run_new.r
@@ -2,10 +2,9 @@
 # This script runs the intertemporal decomposition for the baseline run
 ################################################################################
 
-te_all<-read.csv("ssp_modeling/output_postprocessing/data/emission_targets_bulgaria.csv")
-# filter to keep only the subsectors of interest
+te_all<-read.csv(paste0("ssp_modeling/output_postprocessing/data/emission_targets_",region,"_new.csv"))
 #te_all <- subset(te_all,Subsector%in%c( "lvst","lsmm","agrc","ippu","waso","trww","frst","lndu","soil"))
-target_country <- "BGR" # Bulgaria
+target_country <- iso_code3
 te_all<-te_all[,c("Subsector","Gas","Vars","Edgar_Class",target_country)]
 te_all[,"tvalue"] <- te_all[,target_country]
 te_all[,target_country] <- NULL
@@ -14,14 +13,11 @@ target_vars <- unlist(strsplit(te_all$Vars,":"))
 # modification of AG - Livestock:N2O subsector matching
 te_all$Vars[3] <- "emission_co2e_n2o_lsmm_direct_anaerobic_digester:emission_co2e_n2o_lsmm_direct_anaerobic_lagoon:emission_co2e_n2o_lsmm_direct_composting:emission_co2e_n2o_lsmm_direct_daily_spread:emission_co2e_n2o_lsmm_direct_deep_bedding:emission_co2e_n2o_lsmm_direct_dry_lot:emission_co2e_n2o_lsmm_direct_incineration:emission_co2e_n2o_lsmm_direct_liquid_slurry:emission_co2e_n2o_lsmm_direct_paddock_pasture_range:emission_co2e_n2o_lsmm_direct_poultry_manure:emission_co2e_n2o_lsmm_direct_storage_solid:emission_co2e_n2o_lsmm_indirect_anaerobic_digester:emission_co2e_n2o_lsmm_indirect_anaerobic_lagoon:emission_co2e_n2o_lsmm_indirect_composting:emission_co2e_n2o_lsmm_indirect_daily_spread:emission_co2e_n2o_lsmm_indirect_deep_bedding:emission_co2e_n2o_lsmm_indirect_dry_lot:emission_co2e_n2o_lsmm_indirect_incineration:emission_co2e_n2o_lsmm_indirect_liquid_slurry:emission_co2e_n2o_lsmm_indirect_paddock_pasture_range:emission_co2e_n2o_lsmm_indirect_poultry_manure:emission_co2e_n2o_lsmm_indirect_storage_solid"
 
-
+# data from SiSePuede
 data_all<-read.csv(paste0(dir.output,output.file))
-rall <- unique(data_all$region)
-#check primary ids
-table(data_all$primary_id)
 
-#check time periods
-table(data_all$time_period)
+
+rall <- unique(data_all$region)
 
 #set params of rescaling function
 initial_conditions_id <- "_0"
@@ -35,14 +31,15 @@ dim(data_all)
 te_all$simulation <- 0
 for (i in 1:nrow(te_all))
  {
-    #i <- 1
+   # i<- 12
     vars <- unlist(strsplit(te_all$Vars[i],":"))
     if (length(vars)>1) {
     te_all$simulation[i] <- as.numeric(rowSums(data_all[data_all$primary_id==gsub("_","",initial_conditions_id) &  data_all$time_period==time_period_ref,vars]))
     } else {
      te_all$simulation[i] <- as.numeric(data_all[data_all$primary_id==gsub("_","",initial_conditions_id) &  data_all$time_period==time_period_ref,vars])   
     }
-  }
+}
+
 te_all$simulation <- ifelse(te_all$simulation==0 & te_all$tvalue>0,0,1)
 correct<- aggregate(list(factor_correction=te_all$simulation),list(Edgar_Class=te_all$Edgar_Class),mean)
 te_all <- merge(te_all,correct,by="Edgar_Class")
@@ -58,3 +55,4 @@ z<-1
 rescale(z,rall,data_all,te_all,initial_conditions_id,dir.output,time_period_ref)
 
 print('Finish:run_script_baseline_run_new_asp process')
+
