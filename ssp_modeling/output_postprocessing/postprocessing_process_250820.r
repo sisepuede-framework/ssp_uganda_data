1ac3fc6009e49a791f20e655e07f26acd90310d7
jroblar <jroblar98@gmail.com>
2025-08-27 22:10:07 -0600

    update output directory and year reference in postprocessing scripts

--- ssp_modeling/output_postprocessing/postprocessing_process_250820.r
+++ ssp_modeling/output_postprocessing/postprocessing_process_250820.r
@@ -11,13 +11,13 @@ library(ggplot2)
 rm(list=ls())
 
 #ouputfile
-dir.output  <- "ssp_modeling/ssp_run_output/sisepuede_summary_results_run_sisepuede_run_2025-08-20T13;21;41.290976/"
+dir.output  <- "ssp_modeling/ssp_run_output/sisepuede_summary_results_run_sisepuede_run_2025-08-27T20;12;53.345956/"
 output.file <- "WIDE_INPUTS_OUTPUTS.csv"
 
 region <- "uganda" 
 iso_code3 <- "UGA"
 
-year_ref <- 2020
+year_ref <- 2022
 
 source('ssp_modeling/output_postprocessing/scr/run_script_baseline_run_new.r')
 
