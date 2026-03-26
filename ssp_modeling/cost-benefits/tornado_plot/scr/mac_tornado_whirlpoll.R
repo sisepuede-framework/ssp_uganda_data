ffb944ac437caac35aea38f7fd37fbc61fa69f3d
fabian fuentes <cffuentes@tec.mx>
2026-03-26 11:32:02 -0600

    final update

--- ssp_modeling/cost-benefits/tornado_plot/scr/mac_tornado_whirlpoll.R
+++ ssp_modeling/cost-benefits/tornado_plot/scr/mac_tornado_whirlpoll.R
@@ -7,23 +7,36 @@ rm(list=ls())
 
 #ouputfile
 dir <- "ssp_modeling/ssp_run_output/sisepuede_run_2026-03-10t13;27;53.264959/"
+mac <- fread(paste0(dir, 'ATTRIBUTE_MAP_TORNADO_WHIRLPOOL.csv'))
 
 tornado <- fread(paste0(dir, 'marginal_abatement_costs.csv'))
 
-tornado$mac_tornado <- tornado$marginal_abatement_cost
- 
-tornado <- select(tornado, primary_id, mac_tornado)
+# data for tableau tornado plot
+tableau_tornado <- left_join(mac, 
+                            select(tornado, primary_id,emission_total,base_emission_total,emission_diff,
+                                   technical_cost,marginal_abatement_cost), 
+                            by=c('primary_id_tornado'='primary_id'))
+tableau_tornado <- tableau_tornado %>% filter(emission_diff!=0)                            
+fwrite(tableau_tornado, 'ssp_modeling/Tableau/data/tableau_tornado.csv', row.names = F)
 
+
+tornado$mac_tornado <- tornado$marginal_abatement_cost 
+tornado <- select(tornado, primary_id, mac_tornado)
 
 
 whirlpool <- fread(paste0(dir, 'marginal_abatement_costs_whirlpool.csv'))
 
+# data for tableau whirlpool
+tableau_whirlpool <- left_join(mac, 
+                            select(whirlpool, primary_id,emission_total,base_emission_total,emission_diff,
+                                   technical_cost,marginal_abatement_cost), 
+                            by=c('primary_id_whirlpool'='primary_id'))
+tableau_whirlpool <- tableau_whirlpool %>% filter(emission_diff!=0)                            
+fwrite(tableau_whirlpool, 'ssp_modeling/Tableau/data/tableau_whirlpool.csv', row.names = F)
+
+
 whirlpool$mac_whirlpool <- whirlpool$marginal_abatement_cost
-
 whirlpool <- select(whirlpool, primary_id, mac_whirlpool)
-
-
-mac <- fread(paste0(dir, 'ATTRIBUTE_MAP_TORNADO_WHIRLPOOL.csv'))
 
 mac <- left_join(mac, tornado, by=c('primary_id_tornado'='primary_id'))
 
@@ -32,4 +45,8 @@ mac <- left_join(mac, whirlpool, by=c('primary_id_whirlpool'='primary_id'))
 mac$mac_tornado <- as.numeric(mac$mac_tornado)
 mac$mac_whirlpool <- as.numeric(mac$mac_whirlpool) 
 
-fwrite(mac, paste0(dir,'mac_tornado_to_whirlpool.csv'), row.names = F)
+
+mac <- mac %>% filter(!is.na(mac_tornado) & !is.na(mac_whirlpool))
+
+
+fwrite(mac, 'ssp_modeling/Tableau/data/mac_tornado_to_whirlpool.csv', row.names = F)
