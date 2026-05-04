169d440adf9404eb3c63d2a6a7605bdea0b134e7
Alexa <A00828221@tec.mx>
2026-05-04 13:40:14 -0600

    New runs

--- ssp_modeling/output_postprocessing/postprocessing_process_nr.r
+++ ssp_modeling/output_postprocessing/postprocessing_process_nr.r
@@ -11,7 +11,7 @@ library(ggplot2)
 rm(list=ls())
 
 #ouputfile
-run <- 'sisepuede_summary_results_run_sisepuede_run_2026-04-30T22;47;01.605912'
+run <- 'sisepuede_summary_results_run_sisepuede_run_2026-05-03T23;39;43.045564'
 
 dir.output  <- paste0("ssp_modeling/ssp_run_output/",run,"/")
 #output.file <- "c585e7e9-e32f-4131-999b-ee7fc5ec014e.csv"
@@ -87,6 +87,7 @@ source('ssp_modeling/output_postprocessing/scr/levers_table/#create levers table
 
 # Jobs table
 source('ssp_modeling/output_postprocessing/scr/levers_table/#create jobs table.r')
+
 
 
 
